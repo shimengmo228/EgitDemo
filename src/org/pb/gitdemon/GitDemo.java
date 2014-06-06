51e0be62696a5096eb063330ab19be66eff1a322
shimengmo228 <shimengmo228@gmail.com>
2014-06-07 02:03:20 +0800

    gitdemo commit

---
+++ src/org/pb/gitdemon/GitDemo.java
@@ -0,0 +1,15 @@
+package org.pb.gitdemon;
+
+public class GitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a git demon XD...");
+		return;
+
+	}
+
+}
