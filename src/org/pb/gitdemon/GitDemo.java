56e7856368e1007ab0786993011f1c05ca53b084
shimengmo228 <shimengmo228@gmail.com>
2014-06-07 02:19:46 +0800

    too commit

--- src/org/pb/gitdemon/GitDemo.java
+++ src/org/pb/gitdemon/GitDemo.java
@@ -8,6 +8,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is a git demon XD...");
+		System.out.println("this is a git demon XD, too...");
 		return;
 
 	}
