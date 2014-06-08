01de799397c5ce179adcc2b5445c3837222dedb2
shimengmo228 <shimengmo228@gmail.com>
2014-06-08 09:46:55 +0800

    2014.06.08

--- src/org/pb/gitdemon/GitDemo.java
+++ src/org/pb/gitdemon/GitDemo.java
@@ -9,7 +9,7 @@ public class GitDemo {
 		// TODO Auto-generated method stub
 		System.out.println("this is a git demon XD...");
 		System.out.println("this is a git demon XD, too...");
-		System.out.println("this is a git demon XD, three...");
+		//System.out.println("this is a git demon XD, three...");
 		return;
 	}
 }
